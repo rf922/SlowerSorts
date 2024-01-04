a234f25acf16c16f927d3ab2a71289d78d53b75d
rf922 <rfabiani@sfsu.edu>
2024-01-04 07:35:30 -0800

    refactoring to change SlowerSorts to a functional interface

--- src/SlowerSorts/SortFactory.java
+++ src/SlowerSorts/SortFactory.java
@@ -13,16 +13,17 @@ public class SortFactory {
     /**
      * Returns an instance of a sorting algorithm based on the type passed
      *
+     * @param <T>
      * @param type The type of sorting algorithm
      * @return An instance of the algorithm
      * @throws IllegalArgumentException if an unknown sorting type is specified.
      */
-    public static SlowerSorts<?> getSorter(SortType type) {
+    public static <T extends Comparable<T>> SlowerSorts<T> getSorter(SortType type) {
         switch (type) {
-            case SELECTION_SORT -> { return new SelectionSort<>(); }
+            case SELECTION_SORT -> { return SelectionSort::sort; }
             case INSERTION_SORT -> { return new InsertionSort<>(); }
             case SHELL_SORT -> { return new ShellSort<>(); }
-            default -> throw new IllegalArgumentException("Not Yet Implemented");
+            default -> throw new IllegalArgumentException("Unknown type passed");
         }
     }
 
@@ -32,6 +33,6 @@ public class SortFactory {
     public enum SortType {
         SELECTION_SORT,
         INSERTION_SORT,
-        SHELL_SORT
+        SHELL_SORT        
     }
 }
